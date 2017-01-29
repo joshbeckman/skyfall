8f3412dd007e7d5ddced3df1e03c71cae835ae5f
Josh Beckman <email@andjosh.com>
2017-01-29 17:26:18 -0600

    Upgrade static file serving

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var koa         = require('koa.io'),
     compress    = require('koa-compress'),
     route       = require('koa-route'),
-    staticCache = require('koa-static-cache'),
+    serve       = require('koa-static'),
     path        = require('path'),
     pkg         = require('./package'),
     photon      = require('./photon'),
@@ -10,18 +10,23 @@ var koa         = require('koa.io'),
     app         = koa();
 
 app.use(compress());
-app.use(staticCache(path.join(__dirname, 'public')));
+app.use(serve(path.join(__dirname, 'public')));
 
 app.use(function *(next){
     this.set('x-' + pkg.name + '-version', pkg.version);
     yield next;
 });
 
-photon.init();
-photon.listen(function onData(data) {
-    console.info(data);
-    app.io.emit(data.source, data);
-});
-
 app.listen(port);
 console.info('%s v%s listening on 0.0.0.0:%s', pkg.name, pkg.version, port);
+
+app.io.use(function* (next) {
+    console.log('connect');
+    yield* next;
+    console.log('disconnect');
+});
+
+photon.init();
+photon.listen(function onData(data) {
+    app.io.emit(data.source, data);
+});
