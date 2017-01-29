4e6ddb92033ba195869f9db1e3943063ed5093b7
Josh Beckman <email@andjosh.com>
2017-01-29 16:55:07 -0600

    Get ready for Heroku

--- index.js
+++ index.js
@@ -13,11 +13,8 @@ app.use(compress());
 app.use(staticCache(path.join(__dirname, 'public')));
 
 app.use(function *(next){
-    var start = new Date;
     this.set('x-' + pkg.name + '-version', pkg.version);
     yield next;
-    var ms = new Date - start;
-    console.log('%s %s - %sms', this.method, this.url, ms);
 });
 
 photon.init();
