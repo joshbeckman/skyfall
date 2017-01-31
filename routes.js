cf1ad1a83ac146902040ad490ca59c6281c334f4
Josh Beckman <email@andjosh.com>
2017-01-31 12:34:07 -0600

    Emit socket packets on webhook

--- routes.js
+++ routes.js
@@ -11,7 +11,10 @@ function webhook(app) {
     return route.post('/input/webhook', function* rouote_webhook() {
         var body = yield parse(this);
         console.log(body);
-        app.io.emit('foobar', body);
+        app.io.emit('BARORESISTOR', { unit: 'kPa', value: parseFloat(body.KPA), source: 'BARORESISTOR' });
+        app.io.emit('THERMORESISTOR', { unit: 'C', value: ((parseFloat(body.C) * 1.8) + 32), source: 'THERMORESISTOR' });
+        app.io.emit('PHOTORESISTOR', { unit: 'RL', value: parseFloat(body.RL), source: 'PHOTORESISTOR' });
+        app.io.emit('HUMISTOR', { unit: 'RH', value: parseFloat(body.RH), source: 'HUMISTOR' });
         this.body = {};
     });
 };
