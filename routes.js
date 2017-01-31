0be16786bda1d8c41ce7198e93a2b5ddb5dc5fd2
Josh Beckman <email@andjosh.com>
2017-01-31 12:36:04 -0600

    Separate celcius and fahrenheit

--- routes.js
+++ routes.js
@@ -12,7 +12,8 @@ function webhook(app) {
         var body = yield parse(this);
         console.log(body);
         app.io.emit('BARORESISTOR', { unit: 'kPa', value: parseFloat(body.KPA), source: 'BARORESISTOR' });
-        app.io.emit('THERMORESISTOR', { unit: 'C', value: ((parseFloat(body.C) * 1.8) + 32), source: 'THERMORESISTOR' });
+        app.io.emit('THERMORESISTOR', { unit: 'C', value: parseFloat(body.C), source: 'THERMORESISTOR' });
+        app.io.emit('THERMORESISTOR', { unit: 'F', value: ((parseFloat(body.C) * 1.8) + 32), source: 'THERMORESISTOR' });
         app.io.emit('PHOTORESISTOR', { unit: 'RL', value: parseFloat(body.RL), source: 'PHOTORESISTOR' });
         app.io.emit('HUMISTOR', { unit: 'RH', value: parseFloat(body.RH), source: 'HUMISTOR' });
         this.body = {};
